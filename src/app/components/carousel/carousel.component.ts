13362266c593eeab4773c2c73dacb3a426045d86
gigih santoso <gigih985042@gmail.com>
2024-07-29 09:05:22 +0700

    update carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
+  slides = [
+    '../assets/image-1.jpg',
+    '../assets/image-2.jpg',
+    '../assets/image-3.jpg',
+    '../assets/image-4.jpg',
+    '../assets/image-2.jpg'
+  ];
 
+  currentSlide = 0;
+
+  getTransform() {
+    console.log(`-${this.currentSlide * (100)}%`, this.currentSlide);
+
+    // if (this.currentSlide == 0) {
+    //   return `translateX(0%)`
+    // } else if (this.currentSlide == 1) {
+    //   return `translateX(-66.6666%)`
+    // } else if (this.currentSlide == this.slides.length-1){
+    //   return `translateX(-200%)`
+    // } else {
+    //   return `translateX(-138%)`
+    // }
+    return `translateX(-${this.currentSlide * (100)}%)`;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
 }
